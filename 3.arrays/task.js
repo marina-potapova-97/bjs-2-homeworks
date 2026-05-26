59e5e0d611350af30f3df62868735f7d402f16d9
Marina Potapova <mp051297@yandex.ru>
2026-05-26 20:29:22 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,27 @@
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+	return arr1.every((element, index) => {
+		return element === arr2[index];
+	});
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	const filteredUsers = users.filter(user => user.gender === gender);
+	if (filteredUsers.length === 0) {
+		return 0;
+	}
+	const ageSumAndCount = filteredUsers.reduce((accumulator, currentUser) => {
+		accumulator.sum += currentUser.age;
+		accumulator.count++;
+		return accumulator;
+	}, {
+		sum: 0,
+		count: 0
+	});
+	const averageAge = ageSumAndCount.sum / ageSumAndCount.count;
+
+	return averageAge;
 }
+
